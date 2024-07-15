0ee6391f4392af0da378df5b8d5a38b2068e3ada
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-07-15 08:51:00 -0600

    created a file to deploy the smart contract

---
+++ chainlink_functions/strava/deploy_strava.js
@@ -0,0 +1,23 @@
+require("@chainlink/env-enc").config();
+
+async function main() {
+
+    const subscriptionId = process.env.SUBSCRIPTION_ID;
+
+    const StravaFactory = await ethers.getContractFactory("StravaConsumer");
+    const StravaContract = await StravaFactory.deploy(subscriptionId);
+    
+    console.log('Deploying Contract.');
+  
+    await StravaContract.waitForDeployment();
+  
+    console.log('Contract deployed successfully.');
+    console.log("Contract address:", await StravaContract.getAddress());
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
