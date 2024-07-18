10bdcbba8d4264161a9a277aa619a5a5c3ae8784
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-07-18 13:30:15 -0600

    adjusted the deploy.js file

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,19 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
 async function main() {
 
-  const transactionsFactory = await hre.ethers.getContractFactory("Goalfi");
-  const transactionsContract = await transactionsFactory.deploy();
+    const source = fs.readFileSync(path.resolve(__dirname, 'source.js'), 'utf8');
+    const subscriptionId = process.env.SEPOLIA_SUBSCRIPTION_ID;
 
-  console.log('Deploying Contract.');
+    // Deploy the contract using the factory.
+    const GoalfiFactory = await ethers.getContractFactory("Goalfi");
+    const GoalfiContract = await GoalfiFactory.deploy(subscriptionId, source);
+    
+    console.log('Deploying Contract.');
 
-  await transactionsContract.deployed();
+    // Wait until the contract is deployed.
+    await GoalfiContract.deployed();
+    console.log('Contract deployed successfully.');
+    
+    // Log the address of the deployed contract.
+    console.log("Contract address:", GoalfiContract.address);
+  }
   
-  console.log('Contract deployed successfully.');
-  console.log("Contract address:", transactionsContract.address);
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+    
