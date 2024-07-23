7a9d4ca8b99c0d18fd0cedecf30cdcdf6969c505
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-07-23 09:42:50 -0600

    updated the goal card

--- client/src/components/GoalCard.jsx
+++ client/src/components/GoalCard.jsx
@@ -21,7 +21,7 @@ const GoalCard = ({ goal, showJoinButton, showViewButton, joinGoal }) => {
     <div className="flex flex-col justify-between h-full white-glassmorphism p-6 mx-10 my-4 cursor-pointer hover:shadow-xl rounded-lg border border-gray-700 w-full max-w-sm">
       <div>
         <div className="flex justify-between items-center w-full mb-4">
-          <span className={`text-sm font-semibold px-5 py-2 rounded-full text-white border border-gray-700 shadow-lg ${goal.colour}`}>{goal.category}</span>
+          <span className={`text-sm font-semibold px-5 py-2 rounded-full text-white border border-gray-700 shadow-lg ${goal.iconColor}`}>{goal.category}</span>
           <div className={`w-10 h-10 rounded-full flex justify-center items-center border border-gray-700 ${goal.iconColor}`}>
             {typeof goal.icon === 'string' ? (
               <img src={goal.icon} alt="icon" className="w-full h-full" />
@@ -43,7 +43,7 @@ const GoalCard = ({ goal, showJoinButton, showViewButton, joinGoal }) => {
           </div>
           <div className="flex justify-between mb-2">
             <span>Total Staked</span>
-            <span>{goal.currentDeposits} ETH</span>
+            <span>{goal.currentDeposits} AVAX</span>
           </div>
           <div className="relative pt-6 mb-4 w-full">
             <div className="overflow-hidden h-1 text-xs flex rounded-full bg-gray-700">
@@ -63,7 +63,7 @@ const GoalCard = ({ goal, showJoinButton, showViewButton, joinGoal }) => {
         {showViewButton && (
           <button
             onClick={handleViewGoal}
-            className={`text-white w-full py-2 rounded-full hover:bg-opacity-80 transition duration-200 mt-auto border border-gray-700 shadow-lg ${goal.colour}`}>
+            className={`text-white w-full py-2 rounded-full hover:bg-opacity-80 transition duration-200 mt-auto border border-gray-700 shadow-lg ${goal.buttonColour}`}>
             View Goal
           </button>
         )}
@@ -81,7 +81,7 @@ const GoalCard = ({ goal, showJoinButton, showViewButton, joinGoal }) => {
             <button
               onClick={handleJoinGoal}
               disabled={!amount || parseFloat(amount) <= 0}
-              className={`text-white w-full py-2 rounded-full hover:bg-opacity-80 transition duration-200 mt-auto border border-gray-700 shadow-lg ${goal.colour} mt-2`}>
+              className={`text-white w-full py-2 rounded-full hover:bg-opacity-80 transition duration-200 mt-auto border border-gray-700 shadow-lg ${goal.buttonColour} mt-2`}>
               Join Goal
             </button>
           </>
