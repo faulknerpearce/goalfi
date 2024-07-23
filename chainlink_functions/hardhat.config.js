25a9c7be3dcf777d82905c27136a1b2d54f8cb25
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-07-23 10:05:04 -0600

    added the fuji network

--- chainlink_functions/hardhat.config.js
+++ chainlink_functions/hardhat.config.js
@@ -2,11 +2,24 @@ require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 
 module.exports = {
-  solidity: '0.8.24',
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+      },
+      {
+        version: "0.8.24",
+      },
+    ],
+  },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    fuji: {
+      url: process.env.FUJI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
