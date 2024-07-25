08a5a61c2ce2bb2785e9323419925d1ee0ce39d4
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-07-25 16:33:04 -0600

    refactored files

--- client/src/utils/getGoalsForUser.js
+++ client/src/utils/getGoalsForUser.js
@@ -4,9 +4,6 @@ import { contractABI, contractAddress } from "./constants";
 export const getGoalsForUser = async (currentAccount) => {
   const provider = new ethers.BrowserProvider(window.ethereum);
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-  console.log("Current account:", currentAccount);
-
   const goalCount = await contract.goalCount();
   const userGoals = [];
 
@@ -14,13 +11,12 @@ export const getGoalsForUser = async (currentAccount) => {
     const participants = await contract.getParticipantAddresses(i);
     if (participants.map(p => p.toLowerCase()).includes(currentAccount.toLowerCase())) {
       const userProgress = await contract.getParticipantProgress(i, currentAccount);
-      console.log(`Goal ${i} - User progress: ${userProgress}`);
+      //console.log(`Goal ${i} - User progress: ${userProgress}`);
       userGoals.push({
         goalId: i,
         progress: userProgress,
       });
     }
   }
-  console.log("User goals:", userGoals);
   return userGoals;
 };
