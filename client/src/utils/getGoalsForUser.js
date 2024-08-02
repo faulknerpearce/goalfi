550250222f9988f1951447d1eb81661a8bbd7026
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-08-02 11:17:02 -0600

    inclued the users distance

--- client/src/utils/getGoalsForUser.js
+++ client/src/utils/getGoalsForUser.js
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { contractABI, contractAddress } from "./constants";
 
+// Function to fetch goals associated with a particular user.
 export const getGoalsForUser = async (currentAccount) => {
   const provider = new ethers.BrowserProvider(window.ethereum);
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
@@ -11,10 +12,13 @@ export const getGoalsForUser = async (currentAccount) => {
     const participants = await contract.getParticipantAddresses(i);
     if (participants.map(p => p.toLowerCase()).includes(currentAccount.toLowerCase())) {
       const userProgress = await contract.getParticipantProgress(i, currentAccount);
+      const userDistance = await contract.getUserDistance(currentAccount, i)
       //console.log(`Goal ${i} - User progress: ${userProgress}`);
+      //console.log(`User Distance: ${userDistance}. for Goal ID: ${i}`)
       userGoals.push({
         goalId: i,
         progress: userProgress,
+        userDistance: Number(userDistance)
       });
     }
   }
