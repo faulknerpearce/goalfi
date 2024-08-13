bf83f50710f720a4a21b7c5e57e1730c18418003
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-08-13 09:04:30 -0600

    updated the srouce file to read from multiple users tokens

--- chainlink_functions/scripts_strava/source.js
+++ chainlink_functions/scripts_strava/source.js
@@ -1,26 +1,35 @@
-const accessToken = args[0];
+const dataMap = JSON.parse(args[0]); 
 const activityType = args[1];
-const apiResponse = await Functions.makeHttpRequest({
-  url: 'https://www.strava.com/api/v3/athlete/activities',
-  headers: { Authorization: `Bearer ${accessToken}` },
-  responseType: 'json'
-});
-if (apiResponse.error) {
-  throw Error('Request failed');
-}
-const data = apiResponse.data;
+const results = {};
 
-let totalDistance = 0;
+for (const ID in dataMap) {
+  const accessToken = dataMap[ID];
 
-// Filters activities based on the activity type and ensure they are not manually added.
-const activities = data.filter(activity => activity.sport_type === activityType && !activity.manual);
+  const apiResponse = await Functions.makeHttpRequest({
+    url: 'https://www.strava.com/api/v3/athlete/activities',
+    headers: { Authorization: `Bearer ${accessToken}` },
+    responseType: 'json'
+  });
 
-if (activities) {
+  if (apiResponse.error) {
+    throw Error(`Request failed: ${error}`);
+  }
+
+  const data = apiResponse.data;
+  let totalDistance = 0;
+
+  // Filters activities based on the activity type and ensure they are not manually added.
+  const activities = data.filter(activity => activity.sport_type === activityType );
+
+  if (activities) {
     activities.forEach(activity => {
-        totalDistance += Math.round(Number(activity.distance));
+      totalDistance += Math.round(Number(activity.distance));
     });
-  
-    return Functions.encodeUint256(totalDistance);
-} else {
-    return Functions.encodeString(`No activities found for type: ${activityType}.`);
+
+    results[ID] = totalDistance;
+  } else {
+    results[ID] = 0;
+  }
 }
+
+return Functions.encodeString(JSON.stringify(results));
