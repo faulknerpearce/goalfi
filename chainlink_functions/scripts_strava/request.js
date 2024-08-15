146c00d9deeb2dc491068208fb355e9e09344181
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-08-15 12:15:26 -0600

    updated the request file

--- chainlink_functions/scripts_strava/request.js
+++ chainlink_functions/scripts_strava/request.js
@@ -1,23 +1,23 @@
-const dotenv = require("dotenv");
 const { getContractInstance } = require("./getContractInstance");
-
+const dotenv = require("dotenv");
 dotenv.config();
 
-async function requestActivityData(AccsessToken, activityType, walletAddress, goalId) {
+// Function to request activity data using chainlink functions with the smart contract.
+async function requestActivityData(dataList, activityType) {
   
+  // Get an instance of the smart contract.
   const contract = getContractInstance();
 
   console.log(`Requesting activity data for sport type: ${activityType}` );
 
-  const tx = await contract.executeRequest(AccsessToken, activityType);
-  
+  // Send the activity data request to the smart contract.
+  const tx = await contract.executeRequest(dataList, activityType);
+
   console.log(`Activity data request sent for ${activityType}, transaction hash: ${tx.hash}`);
 }
 
-const AccsessToken = process.env.ACCESS_TOKEN;
+// Hardcoded data for testing. Takes a user id and the access token.
+const userData = `{"1": "${process.env.ACCESS_TOKEN_ONE}", "2": "${process.env.ACCESS_TOKEN_TWO}", "3": "${process.env.ACCESS_TOKEN_THREE}"}`;
+const activity = 'Run';
 
-const activityType = 'Run'; // Hardcoded for testing.
-const walletAddress = '0x96af4089a5bE5e29efB631Fee00FF0b1005985BB';// Hardcoded for testing.
-const goalId = '0';// Hardcoded for testing.
-
-requestActivityData(AccsessToken, activityType, walletAddress, goalId).catch(console.error);
+requestActivityData(userData, activity).catch(console.error);
