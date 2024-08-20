cd7f6cc03a17c38063c287dba7881a8088579f48
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-08-20 12:27:43 -0600

    added a script for getting the contract instance

---
+++ server/main/getContractInstance.js
@@ -0,0 +1,27 @@
+const { ethers } = require("ethers");
+const dotenv = require("dotenv");
+const abi = require("../contract_abi/Goalfi.json");
+
+// Load environment variables from .env file
+dotenv.config();
+
+function getContractInstance() {
+    // Connect to the network.
+    const provider = new ethers.WebSocketProvider(process.env.FUJI_WS);
+    
+    // Create a wallet instance
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+    // Address of the deployed contract
+    const contractAddress = process.env.FIJI_GOALFI_CONTRACT_ADDRESS;
+
+    // ABI of the deployed contract
+    const contractABI = abi.abi;
+
+    // Create a contract instance
+    const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+
+    return contract;
+}
+
+module.exports = { getContractInstance };
