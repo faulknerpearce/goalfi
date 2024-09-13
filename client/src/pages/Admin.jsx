5c06f59bdf3a7397027f781b7ea09e36c9a965e9
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-09-13 10:45:15 -0600

    added page

---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { TransactionContext } from "../context/TransactionContext";
+
+const Admin = () => {
+  const { currentAccount, fetchToken, fetchParticipantsTokens} = useContext(TransactionContext);
+
+  const handleGetToken = async () => {
+    try {
+      const tokens = await fetchToken(currentAccount); 
+      console.log('Fetched tokens:', tokens);
+    } catch (error) {
+      console.error('Error fetching tokens:', error);
+    }
+  };
+
+  const handlefetchAllTokens = async () => {
+    try {
+      const tokens = await fetchParticipantsTokens(11);
+      console.log('Fetched tokens:', tokens);
+    } catch (error) {
+      console.error('Error fetching tokens:', error);
+    }
+  };
+
+  return (
+    <div className="text-white">
+      <button 
+        className="text-white bg-blue-700 rounded-full px-4 py-2" 
+        onClick={handleGetToken}
+      >
+        Fetch Token (Don't Fucking Press it)
+      </button>
+      <button 
+        className="text-white bg-blue-700 rounded-full px-4 py-2" 
+        onClick={handlefetchAllTokens}
+      >
+        Fetch Participants Tokens (Don't Fucking Press this one either)
+      </button>
+    </div>
+  );
+};
+
+export default Admin;
