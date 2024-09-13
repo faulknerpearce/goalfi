71e84129235fc30a8fa1f3821ad29eef4d60b118
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-09-13 10:31:03 -0600

    removed content from the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,41 +1,11 @@
 import React, { useContext } from 'react';
-import { TransactionContext } from "../context/TransactionContext";
+
 
 const About = () => {
-  const { currentAccount, fetchToken, fetchParticipantsTokens} = useContext(TransactionContext);
-
-  const handleGetToken = async () => {
-    try {
-      const tokens = await fetchToken(currentAccount); 
-      console.log('Fetched tokens:', tokens);
-    } catch (error) {
-      console.error('Error fetching tokens:', error);
-    }
-  };
-
-  const handlefetchAllTokens = async () => {
-    try {
-      const tokens = await fetchParticipantsTokens(11);
-      console.log('Fetched tokens:', tokens);
-    } catch (error) {
-      console.error('Error fetching tokens:', error);
-    }
-  };
 
   return (
-    <div className="text-white">
-      <button 
-        className="text-white bg-blue-700 rounded-full px-4 py-2" 
-        onClick={handleGetToken}
-      >
-        Fetch Token (Don't Fucking Press it)
-      </button>
-      <button 
-        className="text-white bg-blue-700 rounded-full px-4 py-2" 
-        onClick={handlefetchAllTokens}
-      >
-        Fetch Participants Tokens (Don't Fucking Press this one either)
-      </button>
+    <div className="flex w-full justify-center items-center">
+      <h1 className="text-3xl sm:text-5xl text-white py-2 text-gradient">About Page</h1>
     </div>
   );
 };
