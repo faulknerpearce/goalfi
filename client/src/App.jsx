0464a01fa53a7ac7384140f5187c95901fde0e67
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-09-13 10:23:57 -0600

    updated the app page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar, Home, Services } from './components';
 import Discover from './pages/Discover';
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
+import Testing from './pages/Admin';
 
 const App = () => (
   <Router>
@@ -15,6 +16,7 @@ const App = () => (
         <Route path="/discover" element={<Discover />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Testing />} />
       </Routes>
     </div>
   </Router>
