0fc6beb6463a8201614b3d19d8bc18c6fc0bb4e5
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-10-17 14:35:40 -0600

    updated the contract address

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Goalfi.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = "0x408ee6cCe325B9e219592e9010f1A5548e569ceC";
+export const contractAddress = "0xde621Ea9b0a43a285207630146eE1FA1CFd95964";
